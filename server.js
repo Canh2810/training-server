ad7d8f9ea69ad0ed523eee075fe0288c913b8226
canhvo <canh.vo@asnet.com.vn>
2023-04-28 10:08:07 +0700

    .

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-
-// Add middleware to serve index.html for non-API routes
-const path = require('path');
-server.use((req, res, next) => {
-  if (req.method === 'GET' && req.headers.accept.indexOf('html') !== -1) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  } else {
-    next();
-  }
-});
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3200;
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+server.listen(port);
