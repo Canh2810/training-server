0ed0400375b6d98b88067c23af768ef3a34cc455
canhvo <canh.vo@asnet.com.vn>
2023-04-28 10:00:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const express = require('express');
+const path = require('path');
 
-server.use(middlewares);
-server.use(router);
+const app = express();
 
-server.listen(port);
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Always serve the index.html file for non-static routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
